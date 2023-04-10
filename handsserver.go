7c448b98cdc409a61da4b1f8df4532a62a153176
Jairo <kidandcat@gmail.com>
2023-04-10 15:54:39 +0200

    up

--- handsserver.go
+++ handsserver.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"runtime"
 
 	"github.com/go-vgo/robotgo"
 	"github.com/gorilla/websocket"
@@ -24,12 +25,20 @@ const (
 	cmdRightUp
 )
 
+type Event struct {
+	Event byte
+	Args  []byte
+}
+
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
 
 func main() {
+	runtime.LockOSThread()
+
+	var events = make(chan Event)
 	fmt.Println("Go server started")
 	robotgo.MouseSleep = 1
 
@@ -41,43 +50,44 @@ func main() {
 			if err != nil {
 				panic(err)
 			}
-			handleMouseEvent(msg[0], msg[1:])
+			events <- Event{msg[0], msg[1:]}
 		}
 	})
 
-	err := http.ListenAndServe(SERVER, nil)
-	if err != nil {
-		panic(err)
-	}
+	go http.ListenAndServe(SERVER, nil)
+	handleMouseEvents(events)
 }
 
-func handleMouseEvent(ev byte, args []byte) {
-	switch ev {
-	case cmdMove:
-		x := 127 - int(args[0])
-		y := 127 - int(args[1])
-		robotgo.MoveRelative(x, y)
-	case cmdLeftClick:
-		robotgo.Click()
-	case cmdRightClick:
-		robotgo.Click("right")
-	case cmdDoubleLeft:
-		robotgo.Click("left", true)
-	case cmdDoubleRight:
-		robotgo.Click("right", true)
-	case cmdMiddleClick:
-		robotgo.Click("center")
-	case cmdScroll:
-		x := 127 - int(args[0])
-		y := 127 - int(args[1])
-		robotgo.ScrollSmooth(y, 1, 1, x)
-	case cmdLeftDown:
-		robotgo.Toggle("left", "down")
-	case cmdLeftUp:
-		robotgo.Toggle("left", "up")
-	case cmdRightDown:
-		robotgo.Toggle("right", "down")
-	case cmdRightUp:
-		robotgo.Toggle("right", "up")
+func handleMouseEvents(recv chan Event) {
+	for {
+		ev := <-recv
+		switch ev.Event {
+		case cmdMove:
+			x := 127 - int(ev.Args[0])
+			y := 127 - int(ev.Args[1])
+			robotgo.MoveRelative(x, y)
+		case cmdLeftClick:
+			robotgo.Click()
+		case cmdRightClick:
+			robotgo.Click("right")
+		case cmdDoubleLeft:
+			robotgo.Click("left", true)
+		case cmdDoubleRight:
+			robotgo.Click("right", true)
+		case cmdMiddleClick:
+			robotgo.Click("center")
+		case cmdScroll:
+			x := 127 - int(ev.Args[0])
+			y := 127 - int(ev.Args[1])
+			robotgo.ScrollSmooth(y, 1, 1, x)
+		case cmdLeftDown:
+			robotgo.Toggle("left", "down")
+		case cmdLeftUp:
+			robotgo.Toggle("left", "up")
+		case cmdRightDown:
+			robotgo.Toggle("right", "down")
+		case cmdRightUp:
+			robotgo.Toggle("right", "up")
+		}
 	}
 }
